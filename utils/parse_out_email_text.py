de9c25a1cbd503edd6c400d3e43dd425f6cbe9a0
Taras Savchyn <trsvchn@gmail.com>
2020-08-07 13:38:17 +0300

    Add Lesson 11

--- utils/parse_out_email_text.py
+++ utils/parse_out_email_text.py
@@ -1,54 +1,44 @@
-#!/usr/bin/python
-
-from nltk.stem.snowball import SnowballStemmer
+# from nltk.stem.snowball import SnowballStemmer
 import string
 
-def parseOutText(f):
-    """ given an opened email file f, parse out all text below the
-        metadata block at the top
-        (in Part 2, you will also add stemming capabilities)
-        and return a string that contains all the words
-        in the email (space-separated) 
-        
-        example use case:
-        f = open("email_file_name.txt", "r")
-        text = parseOutText(f)
-        
-        """
 
+def parse_out_text(f):
+    """Given an opened email file f, parse out all text below the
+    metadata block at the top
+    (in Part 2, you will also add stemming capabilities)
+    and return a string that contains all the words
+    in the email (space-separated)
 
-    f.seek(0)  ### go back to beginning of file (annoying)
+    Example use case:
+    >>> f = open("email_file_name.txt", "r")
+    >>> text = parse_out_text(f)
+    """
+
+    f.seek(0)  # go back to beginning of file (annoying)
     all_text = f.read()
 
-    ### split off metadata
+    # split off metadata
     content = all_text.split("X-FileName:")
     words = ""
     if len(content) > 1:
-        ### remove punctuation
+        # remove punctuation
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
-        ### project part 2: comment out the line below
+        # project part 2: comment out the line below
         words = text_string
 
-        ### split the text string into individual words, stem each word,
-        ### and append the stemmed word to words (make sure there's a single
-        ### space between each stemmed word)
-        
-
-
-
+        # split the text string into individual words, stem each word,
+        # and append the stemmed word to words (make sure there's a single
+        # space between each stemmed word)
 
     return words
 
-    
 
 def main():
-    ff = open("../text_learning/test_email.txt", "r")
-    text = parseOutText(ff)
-    print text
-
+    with open("../lesson-11-text-learning/test_email.txt", "r") as f:
+        text = parse_out_text(f)
+    print(text)
 
 
 if __name__ == '__main__':
     main()
-
